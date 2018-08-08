8db6a188d2ea149201a4d4369321e641d57147d0
Sergey Khalandachev <s.khalandachev@gmail.com>
2018-08-09 00:59:26 +0300

    default constants

---
+++ Editor/Constants.cs
@@ -0,0 +1,11 @@
+namespace SKTools.ScriptingDefineManager
+{
+    public class Constants
+    {
+        public static string DirectoryPathInAssets;
+        public static string AssetDirectoryPathForPresets;
+        public static string AbsoluteDirectoryPathForPresets;
+    }
+}
+
+
